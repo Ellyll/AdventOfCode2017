30ffa3e0fa85c8788282ae5e6bb57f9d7365ee6d
Ellyll <cawsllyffant-git@yahoo.com>
2017-12-15 20:32:56 +0000

    Problem 6 - second half

---
+++ problem06b.fsx
@@ -0,0 +1,38 @@
+#!/usr/bin/fsharpi
+
+let memoryBanks = [| 14 ; 0 ; 15 ; 12 ; 11 ; 11 ; 3 ; 5 ; 1 ; 6 ; 8 ; 4 ; 9 ; 1 ; 8 ; 4 |]
+//let memoryBanks = [| 0 ; 2 ; 7 ; 0 |]
+
+let rec distribute amount index memoryBanks =
+    if amount = 0 then
+        memoryBanks
+    else
+        let nextMBs =
+            memoryBanks
+            |> Array.mapi (fun i x -> if (i = index) then (x + 1) else x)
+        let nextIndex = if (index + 1) >= (Array.length memoryBanks) then 0 else (index + 1)
+        distribute (amount - 1) nextIndex nextMBs
+
+
+let rec getNumberOfConfigurations configs count memoryBanks =
+    if configs |> Set.contains memoryBanks then
+        (count, memoryBanks)
+    else
+        let nextConfigs = Set.add memoryBanks configs
+        let maxIndex =  
+            memoryBanks
+            |> Array.mapi (fun i x -> i, x)
+            |> Array.maxBy snd 
+            |> fst
+        let nextIndex = if (maxIndex+1) >= (Array.length memoryBanks) then 0 else (maxIndex+1)
+        let nextMBs =
+            memoryBanks
+            |> Array.mapi (fun i x -> if i = maxIndex then 0 else x)
+            |> distribute memoryBanks.[maxIndex] nextIndex
+        getNumberOfConfigurations nextConfigs (count + 1) nextMBs
+
+let numberOfConfigs, config = getNumberOfConfigurations (Set.empty) 0 memoryBanks
+printfn "Number of initial steps: %i, config: %A" numberOfConfigs config
+
+let numberOfConfigs2, config2 = getNumberOfConfigurations (Set.empty) 0 config
+printfn "Number of repeat steps: %i, config: %A" numberOfConfigs2 config2
